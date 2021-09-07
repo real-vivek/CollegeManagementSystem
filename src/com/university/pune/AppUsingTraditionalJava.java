fc65af7509a415e29a3f69e21bb73520a18d7a12
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-09-07 17:33:26 +0530

    Added spring 5 jars

--- src/com/university/pune/AppUsingTraditionalJava.java
+++ src/com/university/pune/AppUsingTraditionalJava.java
@@ -5,7 +5,7 @@ public class AppUsingTraditionalJava {
 	public static void main(String[] args) {
 
 		// We can change FergussonCollege to any other object which implements College interface
-		College fc = new SymbiosisCollege();
+		College fc = new FergussonCollege();
 		System.out.println(fc.getStreamsOffered());
 
 	}
